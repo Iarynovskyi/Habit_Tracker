c4eb852447a18e4044affb056b0081c35cbf36c1
IarynovskyiD <dmytro.yarynovskyi.pm.2023@lpnu.ua>
2024-10-04 14:07:45 +0300

    work with MongoDB | error inside

--- app.py
+++ app.py
@@ -1,51 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
-from collections import defaultdict
-import datetime
+from flask import Flask
+from routes import pages
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
 
-habits = ["Test habit", "Test habit 2"]
-completions = defaultdict(list)
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv('MONGO_URI'))
+    app.db = client['Habits']
 
+    app.register_blueprint(pages)
 
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        return [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-
-    return {"date_range": date_range}
-
-
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
-
-    # Переконайтеся, що для вибраної дати є значення
-    completions_list = completions.get(selected_date, [])
-
-    return render_template("index.html", title="HTracker - Home", habits=habits, completions=completions_list,
-                           selected_date=selected_date)
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habit = request.form.get("habit")
-        if habit:
-            habits.append(habit)
-            return redirect(url_for("index"))  # Перенаправлення на основну сторінку
-    return render_template("add_habit.html", title="HTracker - Add habit", selected_date=datetime.date.today())
-
-
-@app.route("/complete", methods=["POST"])
-def complete_habit():
-    date_str = request.form.get("date")
-    habit = request.form.get("habitName")
-    date = datetime.date.fromisoformat(date_str)
-    completions[date].append(habit)
-
-    return redirect(url_for("index", date=date_str))
+    return app
---
+++ routes.py
@@ -0,0 +1,69 @@
+from dateutil.utils import today
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from collections import defaultdict
+import datetime
+import uuid
+
+pages = Blueprint("habits", __name__, template_folder="templates", static_folder="static")
+
+
+@pages.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.datetime):
+        return [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+
+    return {"date_range": date_range}
+
+
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return today - datetime.timedelta(hours=today.hour, minutes=today.minute, seconds=today.second)
+
+
+@pages.route("/")
+def index():
+    print([e for e in current_app.db.habits.find({})])  # Changed pages.db to current_app.db
+    date_str = request.args.get("date")
+    selected_date = datetime.datetime.fromisoformat(date_str) if date_str else datetime.datetime.today()
+
+    habits_on_date = current_app.db.habits.find({"added": {"$lte": selected_date}})
+    completions = [
+        habit["habit"]
+        for habit in current_app.db.completions.find({"date": selected_date})
+    ]
+
+    return render_template("index.html", title="HTracker - Home", habits=habits_on_date, completions=completions,
+                           selected_date=selected_date)
+
+
+@pages.route("/add", methods=["GET", "POST"])
+def add_habit():
+    today = today_at_midnight()
+    if request.method == "POST":
+        try:
+            current_app.db.habits.insert_one(
+                {
+                    "_id": uuid.uuid4().hex,
+                    "added": today,
+                    "name": request.form.get("habit")
+                }
+            )
+            return redirect(url_for("habits.index"))  # Ensure to redirect after adding a habit
+        except Exception as e:
+            print(f"Error adding habit: {e}")  # Add error logging here
+
+    return render_template("add_habit.html", title="HTracker - Add habit", selected_date=today)
+
+
+@pages.route("/complete", methods=["POST"])
+def complete_habit():
+    date_str = request.form.get("date")
+    habit = request.form.get("habitId")
+    date = datetime.datetime.fromisoformat(date_str)
+
+    try:
+        current_app.db.completions.insert_one({"habitId": habit, "date": date})
+    except Exception as e:
+        print(f"Error completing habit: {e}")  # Add error logging here
+
+    return redirect(url_for("habits.index", date=date_str))
